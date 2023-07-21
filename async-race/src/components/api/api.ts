b3ea37d90f327fffc8fa809a28290b3696c23308
l1senochek <lisenochek96@gmail.com>
2023-07-21 16:10:47 +0300

    refactor: update methods with TryCatchMethod

--- async-race/src/components/api/api.ts
+++ async-race/src/components/api/api.ts
@@ -17,9 +17,10 @@ import {
 export class ApiRequest {
   constructor() {}
 
-  test() {
+  async test() {
     // this.clearGarage();
-    this.getAllItems();
+    const dff = await this.getAllItems();
+    console.log(dff, 'dfs');
     this.engineStatus(6, QueryParams.STATUS_STARTED);
     this.engineDriveMode(6);
     this.getWinner(1);
@@ -48,14 +49,30 @@ export class ApiRequest {
   //   return await this.TryCatchFetch<Item[]>(`${Path.GARAGE}`);
   // }
 
-  async getAllItems(): Promise<Item[]> {
+  async TryCatchMethod<T>(method: () => Promise<T>, name: string): Promise<T> {
     try {
-      return await (await fetch(`${Path.GARAGE}`)).json();
+      return await method();
     } catch (error) {
-      console.log('getAllItems', error);
+      console.log(name, error);
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  async getAllItems(): Promise<Item[]> {
+    return await this.TryCatchMethod<Item[]>(async () => {
+      return await (await fetch(`${Path.GARAGE}`)).json();
+    }, 'getAllItems');
+  }
+
+  // async getAllItems(): Promise<Item[]> {
+  //   try {
+  //     return await (await fetch(`${Path.GARAGE}`)).json();
+  //   } catch (error) {
+  //     console.log('getAllItems', error);
+  //     throw error;
+  //   }
+  // }
 
   async clearGarage(): Promise<void> {
     const allItems = await this.getAllItems();
@@ -64,6 +81,17 @@ export class ApiRequest {
       this.deleteItem(item.id);
     });
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async getItems(pageNum: number): Promise<ItemsResponse> {
+  //   return await this.TryCatchMethod<ItemsResponse>(async () => {
+  //     const response = await fetch(`${Path.GARAGE}?_page=${pageNum}&_limit=${QueryParams.GARAGE_LIMIT}`);
+  //     return {
+  //       items: await response.json(),
+  //       count: response.headers.get(HeadersResponse.X_TOTAL_COUNT),
+  //     };
+  //   }, 'getItems');
+  // }
 
   async getItems(pageNum: number): Promise<ItemsResponse> {
     try {
@@ -79,6 +107,12 @@ export class ApiRequest {
     }
   }
 
+  // _______________ с использованием TryCatchMethod ______________
+  // async getItem(id: number): Promise<ItemsResponse> {
+  //   return await this.TryCatchMethod<ItemsResponse>(async () => {
+  //     return await (await fetch(`${Path.GARAGE}/${id}`)).json();
+  //   }, 'getItem');
+  // }
   async getItem(id: number): Promise<ItemsResponse> {
     try {
       // console.log(await (await fetch(`${Path.GARAGE}/${id}`)).json());
@@ -88,6 +122,21 @@ export class ApiRequest {
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async createItem(body: Item): Promise<Item> {
+  //   return await this.TryCatchMethod<Item>(async () => {
+  //     return await (
+  //       await fetch(`${Path.GARAGE}`, {
+  //         method: HttpMetods.POST,
+  //         headers: {
+  //           'Content-Type': 'appliItemion/json',
+  //         },
+  //         body: JSON.stringify(body),
+  //       })
+  //     ).json();
+  //   }, 'createItem');
+  // }
 
   async createItem(body: Item): Promise<Item> {
     try {
@@ -106,6 +155,17 @@ export class ApiRequest {
     }
   }
 
+  // _______________ с использованием TryCatchMethod ______________
+  // async deleteItem(id: number): Promise<void> {
+  //   return await this.TryCatchMethod<void>(async () => {
+  //     await (
+  //       await fetch(`${Path.GARAGE}/${id}`, {
+  //         method: HttpMetods.DELETE,
+  //       })
+  //     ).json();
+  //   }, 'deleteItem');
+  // }
+
   async deleteItem(id: number): Promise<void> {
     try {
       await (
@@ -118,6 +178,21 @@ export class ApiRequest {
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async updateItem(id: number, body: Item): Promise<Item> {
+  //   return await this.TryCatchMethod<Item>(async () => {
+  //     return await (
+  //       await fetch(`${Path.GARAGE}/${id}`, {
+  //         method: HttpMetods.PUT,
+  //         headers: {
+  //           'Content-Type': 'appliItemion/json',
+  //         },
+  //         body: JSON.stringify(body),
+  //       })
+  //     ).json();
+  //   }, 'updateItem');
+  // }
 
   async updateItem(id: number, body: Item): Promise<Item> {
     try {
@@ -136,6 +211,16 @@ export class ApiRequest {
     }
   }
 
+  // _______________ с использованием TryCatchMethod ______________
+  // async engineStatus(id: number, status: QueryParams): Promise<EngineResponse> {
+  //   return await this.TryCatchMethod<EngineResponse>(async () => {
+  //     return await (
+  //       await fetch(`${Path.ENGINE}?id=${id}&status=${status}`, {
+  //         method: HttpMetods.PATCH,
+  //       })
+  //     ).json();
+  //   }, 'engineStatus');
+  // }
   // engineStatus
   async engineStatus(id: number, status: QueryParams): Promise<EngineResponse> {
     try {
@@ -152,31 +237,16 @@ export class ApiRequest {
     }
   }
 
-  // async startItemEngine(id: number): Promise<EngineResponse> {
-  //   try {
-  //     const response = await (
-  //       await fetch(`${Path.ENGINE}?id=${id}&status=started`, {
-  //         method: HttpMetods.PATCH,
-  //       })
-  //     ).json();
-  //     console.log('222222222222', response);
-  //     return response;
-  //   } catch (error) {
-  //     console.log('startItemEngine', error);
-  //     throw error;
-  //   }
+  // _______________ с использованием TryCatchMethod ______________
+  // async engineDriveMode(id: number): Promise<EngineDriveModeResponse> {
+  //   return await this.TryCatchMethod<EngineDriveModeResponse>(async () => {
+  //     const response = await fetch(`${Path.ENGINE}?id=${id}&status=${QueryParams.STATUS_DRIVE}`, {
+  //       method: 'PATCH',
+  //     });
+  //     return response.status !== 200 ? { success: false } : { ...(await response.json()) };
+  //   }, 'engineDriveMode');
   // }
 
-  // async stopItemEngine(id: number) {
-  //   const response = await (
-  //     await fetch(`${Path.ENGINE}?id=${id}&status=stopped`, {
-  //       method: HttpMetods.PATCH,
-  //     })
-  //   ).json();
-  //   console.log('333333333333', response);
-
-  //   return response;
-  // }
   async engineDriveMode(id: number): Promise<EngineDriveModeResponse> {
     try {
       const response = await fetch(`${Path.ENGINE}?id=${id}&status=${QueryParams.STATUS_DRIVE}`, {
@@ -188,6 +258,19 @@ export class ApiRequest {
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async getWinners(page: number, limit: number, sort?: Sort, order?: Order): Promise<Winners> {
+  //   return await this.TryCatchMethod<Winners>(async () => {
+  //     const response = await fetch(
+  //       `${Path.WINNERS}?_page=${page}&_limit=${limit}${sort && order ? `&_sort=${sort}&_order=${order}` : ``}`
+  //     );
+  //     return {
+  //       data: await response.json(),
+  //       count: response.headers.get(HeadersResponse.X_TOTAL_COUNT),
+  //     };
+  //   }, 'getWinners');
+  // }
 
   async getWinners(page: number, limit: number, sort?: Sort, order?: Order): Promise<Winners> {
     try {
@@ -204,6 +287,13 @@ export class ApiRequest {
     }
   }
 
+  // _______________ с использованием TryCatchMethod ______________
+  // async getWinner(id: number): Promise<Winner> {
+  //   return await this.TryCatchMethod<Winner>(async () => {
+  //     return await (await fetch(`${Path.WINNERS}/${id}`)).json();
+  //   }, 'getWinner');
+  // }
+
   async getWinner(id: number): Promise<Winner> {
     try {
       return await (await fetch(`${Path.WINNERS}/${id}`)).json();
@@ -212,6 +302,21 @@ export class ApiRequest {
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async createWinner(body: Winner): Promise<Winner> {
+  //   return await this.TryCatchMethod<Winner>(async () => {
+  //     return (
+  //       await fetch(`${Path.WINNERS}`, {
+  //         method: HttpMetods.POST,
+  //         body: JSON.stringify(body),
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //       })
+  //     ).json();
+  //   }, 'createWinner');
+  // }
 
   async createWinner(body: Winner): Promise<Winner> {
     try {
@@ -230,6 +335,17 @@ export class ApiRequest {
     }
   }
 
+  // _______________ с использованием TryCatchMethod ______________
+  // async deleteWinner(id: number): Promise<void> {
+  //   return await this.TryCatchMethod<void>(async () => {
+  //     return (
+  //       await fetch(`${Path.WINNERS}/${id}`, {
+  //         method: 'DELETE',
+  //       })
+  //     ).json();
+  //   }, 'deleteWinner');
+  // }
+
   async deleteWinner(id: number): Promise<void> {
     try {
       return (
@@ -242,6 +358,21 @@ export class ApiRequest {
       throw error;
     }
   }
+
+  // _______________ с использованием TryCatchMethod ______________
+  // async updateWinner(id: number, body: Winner): Promise<Winner> {
+  //   return await this.TryCatchMethod<Winner>(async () => {
+  //     return (
+  //       await fetch(`${Path.WINNERS}/${id}`, {
+  //         method: HttpMetods.PUT,
+  //         body: JSON.stringify(body),
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //       })
+  //     ).json();
+  //   }, 'updateWinner');
+  // }
 
   async updateWinner(id: number, body: Winner): Promise<Winner> {
     try {
